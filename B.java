733ef063c021a39fba1335f79fd13e85a070463e
Andy Do <doandy814@gmail.com>
2022-06-29 02:26:19 -0400

    Initial commit

---
+++ B.java
@@ -0,0 +1,15 @@
+// B.java
+class A
+{
+    public A(int i);
+}
+
+public class B extends A
+{
+    public B(int i);
+
+    public static void main(Sting args[])
+    {
+        B b = new B();
+    }
+}
